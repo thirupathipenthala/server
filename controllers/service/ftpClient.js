4e2aaf1a784e5a5d609551dc2139ac96e2be5921
thirupathipenthala <thirupathireddy442@gmail.com>
2021-05-10 14:35:30 +0530

    service

---
+++ controllers/service/ftpClient.js
@@ -0,0 +1,17 @@
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
+const { readFileSync } = require('fs');
+sftp.connect({
+    host: '206.189.140.174',
+    port: 22, // Normal is 22 port
+    username: 'iot',
+    password: 'iotpsswd',
+    privateKey: readFileSync('D:/15react/iot_pri_pt.ppk')
+    // You can use a key file too, read the ssh2 documentation
+}).then(() => {
+    return sftp.list('/home/iot/Binaryfile/');
+}).then(data => {
+    console.log(data, 'the data info');
+}).catch(err => {
+    console.log(err, 'catch error');
+});
